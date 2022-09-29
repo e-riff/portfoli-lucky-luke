40bdaa879052558661d0464e36272abe069fba46
Hugo David <hugodvd@outlook.com>
2022-09-29 17:29:35 +0200

    about step2

--- _about.php
+++ _about.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Lucky Luke</title>
-</head>
-<body>
-    <section id="About"> 
+<section id="About"> 
     <div class="container">
 <div class="description">
     <section id="pictureLuckyLuke">
@@ -17,10 +8,11 @@
 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     Doloremque dolorum modi rerum architecto odit officiis porro minus quam, 
     nemo autem laborum repellendus magni nobis sequi ex reiciendis delectus molestiae doloribus.</p>
+    <section class="friendLuckyLuke">
+    <img alt="picto presentation Lucky Luke" src="/pictures/jolyjymper.jpeg">
+
+
 </div>
 <!-- this a comment -->
     </div>
     </section>
-    </div>
-</body>
-</html>
