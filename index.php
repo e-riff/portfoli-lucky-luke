f4d06f11ba35a31b55d7ea271900d8603a6c6040
Emeric <emeric.riff@gmail.com>
2022-09-29 15:39:45 +0200

    navbar working

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="An old fashionned Portfolio for the most famous cowboy !">
     <link rel="stylesheet" href="/css/style.css">
     <title>Lucky Luke's portfolio</title>
     <link rel="shortcut icon" type="image/png" href="/pictures/favicon.png">
