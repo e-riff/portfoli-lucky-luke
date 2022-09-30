161c607f06278c0dd3685dd55accef41d43d53f5
Hugo David <hugodvd@outlook.com>
2022-09-30 14:02:11 +0200

    Section about step3

--- _about.php
+++ _about.php
@@ -2,14 +2,16 @@
     <div class="container">
 <div class="description">
     <section id="pictureLuckyLuke">
-        <img alt=" Photo de presentation de Lucky luke" src="/pictures/image-luky-luke-1.jpeg">
+        <img alt=" Photo de presentation de Lucky luke" src="/pictures/lucky_luke.png">
     </section>
 <h1>About Lucky Luke</h1>
 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     Doloremque dolorum modi rerum architecto odit officiis porro minus quam, 
     nemo autem laborum repellendus magni nobis sequi ex reiciendis delectus molestiae doloribus.</p>
     <section class="friendLuckyLuke">
-    <img alt="picto presentation Lucky Luke" src="/pictures/jolyjymper.jpeg">
+    <img alt="jolyjumper" src="/pictures/jolly-jumper.png">
+    <img alt="rantanplan" src="/pictures/rantanplan.png">
+    <img alt="joedalton" src="/pictures/joe_dalton.png">
 
 
 </div>
