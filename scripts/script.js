2bd3e82c6d73d5e030e63f9caa2d798bf2996e3a
e-riff <emeric.riff@gmail.com>
2022-09-30 21:23:02 +0200

    navBar appearing

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,11 @@
 const burger = document.querySelector(".burger");
-const topMenu = document.querySelector(".topMenu");
 const sidebar = document.querySelector("sidebar");
 
 
 
 let isDisplayed = false;
 
-
+//// Cette fonction affiche le menu latéral au clic sur le burger ////
 burger.addEventListener('click', function () {
     if (!isDisplayed) {
         sidebar.style.display = "initial";
@@ -17,8 +16,9 @@ burger.addEventListener('click', function () {
     isDisplayed = !isDisplayed;
 });
 
+////cette fonction fait disparaitre le menu après un clic sur une partie///
 sidebar.addEventListener('click', function () {
-    if (window.innerWidth < 768) {
+    if (window.innerWidth < 768) {  //mais seulement sur petit écran (sinon la barre reste affichée)
         if (!isDisplayed) {
             sidebar.style.display = "initial";
         }
@@ -28,3 +28,29 @@ sidebar.addEventListener('click', function () {
         isDisplayed = !isDisplayed;
     }
 });
+
+let flag = false;
+
+window.onscroll = () => {
+    if (scrollY > 600 && flag == false) {
+        sidebar.classList.toggle("hidden");
+        //sidebar.style.display = "inline.flex"
+        flag = true;
+    }
+    else if (scrollY < 600 && flag == true) {
+        // sidebar.style.display = "none"
+        sidebar.classList.toggle("hidden");
+        flag = !flag;
+    }
+};
+
+
+/*
+///Dealing with scroll bar///
+
+if (windows.scrollY <= 200) {
+    sidebar.style.display = "none";
+}
+else {
+    nav.className = sidebar.style.display = "initial";
+}*/
