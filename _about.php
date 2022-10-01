21efcd28969e3b7b1e3389c3667ad68d1d957ce8
Hugo David <hugodvd@outlook.com>
2022-10-01 14:53:42 +0200

    about STEP_4

--- _about.php
+++ _about.php
@@ -1,13 +1,14 @@
-<section id="About">
+<section class="About">
     <div class="container">
 <div class="description">
     <section id="pictureLuckyLuke">
         <img alt=" Photo de presentation de Lucky luke" src="/pictures/lucky_luke.png">
     </section>
 <h1>About Lucky Luke</h1>
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Doloremque dolorum modi rerum architecto odit officiis porro minus quam, 
-    nemo autem laborum repellendus magni nobis sequi ex reiciendis delectus molestiae doloribus.</p>
+<p> Cow-boy solitaire loin de chez lui,
+    il parcourt le far-west à la recherche d'aventures. 
+    On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
+    fait appel à lui à chaque fois que les Dalton s'évadent.</p>
     <section class="friendLuckyLuke">
     <img alt="jolyjumper" src="/pictures/jolly-jumper.png">
     <img alt="rantanplan" src="/pictures/rantanplan.png">
