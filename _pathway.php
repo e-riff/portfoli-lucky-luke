39412dd2d533759b06998c71f061832667197191
axl-crz <acrozier15@icloud.com>
2022-10-04 12:34:32 +0200

    Upload _pathwayand _pathway.css

--- _pathway.php
+++ _pathway.php
@@ -80,4 +80,89 @@
                 </li>
             </ul>
     </div>
+    <div class="container-desktop">
+        <div class="picto-desktop"></div>
+        <div class="heading-desktop">
+            <h2>Parcours Professionnel</h2>
+        </div>
+            <ul class="timeline-desktop">
+                <li>
+                    
+                    <div class="timeline-panel-container-desktop">
+                        <div class="timeline-panel-desktop">
+                            <div class="timeline-heading-desktop">
+                                <h3>Daisy Town</h3>
+                                <h4 class="text-muted-desktop">Agent d'escorte du pénitencier</h4>
+                                <div class="timeline-badge-desktop"></div> 
+                            </div>
+                            <div class="timeline-body-desktop">
+                                <div class="wrapper-desktop">
+                                    <p>Transport des prisonniers</p>
+                                    <p>Passage à tabac des moins coopératifs</p>
+                                    <p>Chasse des évadés</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="timeline-panel-container-inverted-desktop">
+                        <div class="timeline-panel-desktop">
+                            <div class="timeline-body-inverted-desktop">
+                                <div class="wrapper-desktop">
+                                    <p>Installation des poteaux télégraphe</p>
+                                    <p>Montage des installations</p>
+                                    <p>Explications du code linguistique</p>
+                                </div>
+                            </div>
+                            <div class="timeline-heading-inverted-desktop">
+                                <h3>Daisy Town</h3>
+                                <h4 class="text-muted-desktop">Monteur en télécommunication</h4>
+                                <div class="timeline-badge-desktop"></div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    
+                    <div class="timeline-panel-container-desktop">
+                        <div class="timeline-panel-desktop">
+                            <div class="timeline-heading-desktop">
+                                <h3>Pony Express</h3>
+                                <h4 class="text-muted-desktop">Chauffeur-Livreur</h4>
+                                <div class="timeline-badge-desktop"></div>
+                            </div>
+                            <div class="timeline-body-desktop">
+                                <div class="wrapper-desktop">
+                                    <p>Préparation de la tournée</p>
+                                    <p>Chargement du canasson</p>
+                                    <p>Distribution du courrier et dépôt <br>des PRS (Points Relais Saloon)</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    
+                    <div class="timeline-panel-container-inverted-desktop">
+                        <div class="timeline-panel-desktop">
+                            <div class="timeline-body-inverted-desktop">
+                                <div class="wrapper-desktop">
+                                    <p>Transport du bétail</p>
+                                    <p>Nettoyage des enclos</p>
+                                    <p>Dressage du chien de troupeau</p>
+                                </div>
+                            </div>
+                            <div class="timeline-heading-inverted-desktop">
+                                <h3>Freelance</h3>
+                                <h4 class="text-muted-desktop">Conducteur de bétail</h4>
+                                <div class="timeline-badge-desktop"></div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </li>
+            </ul>
+    </div>
 </section>
