bf397be74d918beca3c89c870ca1b682b274f2ff
Hugo David <hugodvd@outlook.com>
2022-10-04 12:32:33 +0200

    about st5 version desktop non finie

--- _about.php
+++ _about.php
@@ -1,19 +1,33 @@
 <section id="About">
     <div class="about-container">
-<div class="description">
-    <section id="pictureLuckyLuke">
-        <img alt=" Photo de presentation de Lucky luke" src="/pictures/lucky_luke.png">
-    </section>
-<h1>About Lucky Luke</h1>
-<p> Cow-boy solitaire loin de chez lui,
-    il parcourt le far-west à la recherche d'aventures. 
-    On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
-    fait appel à lui à chaque fois que les Dalton s'évadent.</p>
-    <section class="friendLuckyLuke">
-    <img alt="jolyjumper" src="/pictures/jolly-jumper.png">
-    <img alt="rantanplan" src="/pictures/rantanplan.png">
-    <img alt="joedalton" src="/pictures/joe_dalton.png">
 
+        <div class="pictureLuckyLuke">
+           <!-- <img alt=" Photo de presentation de Lucky luke" src="/pictures/lucky_luke.png"> -->
         </div>
+        <div class="desktop_description"> 
+             <h2>About Lucky Luke</h2>
+            <p> Cow-boy solitaire loin de chez lui,
+            il parcourt le far-west à la recherche d'aventures. 
+            On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
+            fait appel à lui à chaque fois que les Dalton s'évadent.
+            Cow-boy solitaire loin de chez lui,
+            il parcourt le far-west à la recherche d'aventures. 
+            On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
+            fait appel à lui à chaque fois que les Dalton s'évadent.
+            Cow-boy solitaire loin de chez lui,
+            il parcourt le far-west à la recherche d'aventures. 
+            On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
+            fait appel à lui à chaque fois que les Dalton s'évadent.
+            Cow-boy solitaire loin de chez lui,
+            il parcourt le far-west à la recherche d'aventures. 
+            On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
+            fait appel à lui à chaque fois que les Dalton s'évadent.</p>
+            <div class="friendLuckyLuke">
+                <img alt="jolyjumper" src="/pictures/jolly-jumper.png">
+                <img alt="rantanplan" src="/pictures/rantanplan.png">
+                <img alt="joedalton" src="/pictures/joe_dalton.png">
+            </div>
+        </div>
+        
     </div>
 </section>
