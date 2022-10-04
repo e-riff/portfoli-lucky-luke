54fa1fd9cfffb123f2c09f06b0e5c04e57012cf8
e-riff <emeric.riff@gmail.com>
2022-10-04 17:46:17 +0200

    Title

--- _home.php
+++ _home.php
@@ -1,10 +1,8 @@
 <section id="home">
     <!--     <h1> Welcome </h1> -->
-    <div class="welcome" contenteditable="true">
+    <div class="welcome">
         <h1 class="lucky">Lucky Luke</h1>
-        <h2 class="poor"> A poor lonseome cowboy</h2>
+        <h2 class="poor"> I'm a poor lonseome cowboy</h2>
     </div>
-
-
 
 </section>
