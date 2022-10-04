3d6adeefb8148675b2b66612a0d7faf86c8aa423
e-riff <emeric.riff@gmail.com>
2022-10-04 17:28:51 +0200

    titre 1

--- _home.php
+++ _home.php
@@ -1,7 +1,8 @@
 <section id="home">
     <!--     <h1> Welcome </h1> -->
-    <div class="bubble">
-        <h1 class="welcome">Welcome !!!</h1>
+    <div class="welcome" contenteditable="true">
+        <h1 class="lucky">Lucky Luke</h1>
+        <h2 class="poor"> A poor lonseome cowboy</h2>
     </div>
 
 
