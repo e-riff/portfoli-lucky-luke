acd3d8d74e6d632ff991a7b0a01fb8e608cff7ec
e-riff <emeric.riff@gmail.com>
2022-10-04 12:11:08 +0200

    debut navbar suivant section

--- scripts/script.js
+++ scripts/script.js
@@ -3,41 +3,79 @@ const sidebar = document.querySelector("sidebar");
 
 
 //// Flags de l'affichage des menus ////
-let isDisplayedOnTop = false;
-let isDisplayedOnSide = false;
+const isDisplayed = {
+    barOnTop: false,
+    barOnSide: false,
+    about: false,
+    pathway: false,
+    skills: false,
+};
+
+
+/* let isDisplayedOnTop = false;
+let isDisplayedOnSide = false; */
 
 
 
 //// Cette fonction affiche le menu latéral au clic sur le burger ////
 burger.addEventListener('click', function () {
     sidebar.classList.toggle("displayingLeft");
-    isDisplayedOnSide = !isDisplayedOnSide;
+    isDisplayed.barOnSide = !isDisplayed.barOnSide;
 });
 
 ////cette fonction fait disparaitre le menu après un clic sur une partie////
 sidebar.addEventListener('click', function () {
     if (window.innerWidth < 768) {  //mais seulement sur petit écran (sinon la barre reste affichée)
         sidebar.classList.toggle("displayingLeft");
-        isDisplayedOnSide = !isDisplayedOnSide;
+        isDisplayed.barOnSide = !isDisplayed.barOnSide;
     }
 });
 
 
 
-////cette fonction fait apparaitre le menu du haut après le scroll de la première partie...////
-window.onscroll = () => {
-    if (window.innerWidth > 768) {  //...mais seulement sur version desktop
-        if ((scrollY > 600 && !isDisplayedOnTop) || (scrollY < 600 && isDisplayedOnTop)) {
-            sidebar.classList.toggle("displaying");
-            isDisplayedOnTop = !isDisplayedOnTop;
-        }
+function update() {
+    //Verification de l'emplacement de la section about
+    const about = document.querySelector('#About');
+    const posAbout = about.getBoundingClientRect();
+
+    //Verification de l'emplacement de la section pathway
+    const pathway = document.querySelector('#pathway');
+    const posPathway = pathway.getBoundingClientRect();
+
+    //Verification de l'emplacement de la section skills
+    const skills = document.querySelector('#skills');
+    const posSkills = skills.getBoundingClientRect();
+
+    //J'affiche le menu si j'arrive à la section about, avec une couleur associée
+    if (posAbout.top < 100 && !isDisplayed.barOnTop || posAbout.top > 100 && isDisplayed.barOnTop) {
+        sidebar.classList.toggle("displaying");
+        sidebar.style.backgroundColor = "var(--secondary-color)";
+        isDisplayed.barOnTop = !isDisplayed.barOnTop;
+        isDisplayed.about = !isDisplayed.about;
     }
-};
+    else if (posPathway.top < 100 && isDisplayed.about || posPathway.bottom > 100 && isDisplayed.skills) {
+        isDisplayed.about = !isDisplayed.about;
+        isDisplayed.pathway = !isDisplayed.pathway;
+        sidebar.style.backgroundColor = "green";
+    }
+    else if (posSkills.top < 100 && isDisplayed.pathway) {
+        isDisplayed.pathway = !isDisplayed.pathway;
+        isDisplayed.skills = !isDisplayed.skills;
+        sidebar.style.backgroundColor = "#c35450";
+    }
+
+}
+
+document.addEventListener('scroll', update);
+update();
 
 
+
+
+
+/*
 
 //// Cette fonction pour le formulaire ////
-// const message = document.querySelector("#submitMessage");
 const contactForm = document.querySelector(".contactform");
 const name = document.querySelector("#name");
 
@@ -45,10 +83,12 @@ contactForm.onsubmit = function (event) {
     event.preventDefault();
     const newLine = document.createElement('p');
     contactForm.appendChild(newLine);
-    newLine.innerHTML = `Bonjour ${name.value}, votre message a bien été envoyé ! `;
+    newLine.innerHTML = `Bonjour ${ name.value }, votre message a bien été envoyé! `;
     newLine.classList.add("reply");
 
     setTimeout(function () {
         newLine.remove();
     }, 3000);
 };
+
+*/
