7b2e6d806f432216ad38ef86468e7e80ba1e2ce3
e-riff <emeric.riff@gmail.com>
2022-10-04 23:23:23 +0200

    new homepage

--- _home.php
+++ _home.php
@@ -1,8 +1,9 @@
 <section id="home">
-    <!--     <h1> Welcome </h1> -->
     <div class="welcome">
-        <h1 class="lucky">Lucky Luke</h1>
-        <h2 class="poor"> I'm a poor lonseome cowboy</h2>
+        <h1 class="lucky">Lucky <br> Luke</h1>
+        <h2 class="poor">
+            I'm a poor lonseome cowboy
+        </h2>
     </div>
 
 </section>
