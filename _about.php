241a21a73e1c420f48f0ad194d5eb0ba31f67920
Maria De Sousa Ardiet <mdg.desousa@gmail.com>
2022-10-05 10:58:54 +0200

    modification image header + taille police h2 about

--- _about.php
+++ _about.php
@@ -2,32 +2,24 @@
     <div class="about-container">
 
         <div class="pictureLuckyLuke">
-           <!-- <img alt=" Photo de presentation de Lucky luke" src="/pictures/lucky_luke.png"> -->
+            <!-- <img alt=" Photo de presentation de Lucky luke" src="/pictures/lucky_luke.png"> -->
         </div>
-        <div class="desktop_description"> 
-             <h2>About Lucky Luke</h2>
-            <p> Cow-boy solitaire loin de chez lui,
-            il parcourt le far-west à la recherche d'aventures. 
-            On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
-            fait appel à lui à chaque fois que les Dalton s'évadent.
-            Cow-boy solitaire loin de chez lui,
-            il parcourt le far-west à la recherche d'aventures. 
-            On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
-            fait appel à lui à chaque fois que les Dalton s'évadent.
-            Cow-boy solitaire loin de chez lui,
-            il parcourt le far-west à la recherche d'aventures. 
-            On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
-            fait appel à lui à chaque fois que les Dalton s'évadent.
-            Cow-boy solitaire loin de chez lui,
-            il parcourt le far-west à la recherche d'aventures. 
-            On le reconnaît car c'est l'homme qui tire plus vite que son ombre. On 
-            fait appel à lui à chaque fois que les Dalton s'évadent.</p>
+        <div class="desktop_description">
+            <h2>L'homme qui tire plus vite que son ombre.</h2>
+            <p> Cow-boy solitaire loin de chet moi, je parcours le far-west à la recherche d'aventures.</p>
+            <p> Bien que mon métier soit théoriquement celui de conduire des troupeaux de bétail d'une ville à une autre,
+                je suis souvent appelé soit par certains de mes amis, soit directement par le gouvernement américain,
+                pour remplir des missions quand je ne les dicte pas moi-même dans le souci de rendre justice dans les villes
+                que je traverse.
+            <p>Le plus souvent tout de même, il suis chargé de ramener au pénitencier des bagnards
+                échappés, et en particulier Les Dalton et Billy the Kid.</p>
+
             <div class="friendLuckyLuke">
                 <img alt="jolyjumper" src="/pictures/jolly-jumper.png">
                 <img alt="rantanplan" src="/pictures/rantanplan.png">
                 <img alt="joedalton" src="/pictures/joe_dalton.png">
             </div>
         </div>
-        
+
     </div>
 </section>
